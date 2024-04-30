8162f4eee061af6e5e1b7d14550b586207033d12
Josep Maria Vila <156566553+JosepMariaVila@users.noreply.github.com>
2024-04-30 22:16:06 +0200

    Add files via upload

---
+++ signsubmit.js
@@ -0,0 +1,71 @@
+// ******************** SIGN TRANSACTION OFFLINE ********
+
+async function sign1() {
+  const wallet = xrpl.Wallet.fromSeed(standbySeedField.value);
+  const amount = standbyAmountField.value;
+  const seq = Number(sequenceField.value);
+
+  const txJSON = {
+    Account: wallet.address,
+    TransactionType: "Payment",
+    Destination: standbyDestinationField.value,
+    Amount: xrpl.xrpToDrops(amount),
+    Fee: "14",
+    Sequence: seq,
+  };
+
+  const signed = wallet.sign(txJSON);
+
+  console.log("tx blob is:", signed.tx_blob);
+  console.log("tx hash is:", signed.hash);
+  standbyResultField.value =
+    "tx blob is: " + signed.tx_blob + "\ntx hash is: \n" + signed.hash;
+} // End of sign1()
+
+async function reload() {
+  window.location.reload();
+}
+
+// ************* GET PREFERED NETWORK **************
+
+function getNet() {
+  let net;
+  if (document.getElementById("tn").checked)
+    net = "wss://s.altnet.rippletest.net:51233";
+  if (document.getElementById("dn").checked)
+    net = "wss://s.devnet.rippletest.net:51233";
+  if (document.getElementById("mn").checked) net = "wss://xrplcluster.com";
+  return net;
+} // End of getNet()
+
+// ******************** SUBMIT TRANSACTION ONLINE ********
+
+async function submit1() {
+  let net = getNet();
+
+  standbyResultField1.value = "Connecting to " + getNet() + "....";
+  const client = new xrpl.Client(net);
+  await client.connect();
+
+  const submitonline = await client.submit(txblobField.value);
+
+  console.log(submitonline);
+  document.getElementById("standbyResultField1").value =
+    "Results:" +
+    "\nAccepted: " +
+    submitonline.result.accepted +
+    "\nEngine_result: " +
+    submitonline.result.engine_result +
+    "\nEngine_result_message: " +
+    submitonline.result.engine_result_message +
+    `\nValidated_ledger_index: ` +
+    submitonline.result.validated_ledger_index +
+    "\nHash: " +
+    submitonline.result.tx_json.hash;
+
+  client.disconnect();
+} // End of submit1()
+
+async function reload2() {
+  window.location.reload();
+}
